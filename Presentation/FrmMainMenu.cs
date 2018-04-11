c66eb2d0ffde13a37e31c4bd59afd2ddfc6c3060
Pelle <joaquinpellegrino@gmail.com>
2018-04-11 20:17:43 -0300

    agrego el menu principal. LLeva a los demas forms

---
+++ Presentation/FrmMainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class FrmMainMenu : Form
+    {
+        public FrmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProyect_Click(object sender, EventArgs e)
+        {
+            FrmSelectionProyect formProyect = new FrmSelectionProyect();
+
+            formProyect.Show();
+            Hide();
+        }
+
+        private void FrmMainMenu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Presentation/FrmSelectionProyect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class FrmSelectionProyect : Form
+    {
+        public FrmSelectionProyect()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddProyect_Click(object sender, EventArgs e)
+        {
+            FrmAddProyect frmAddProyect = new FrmAddProyect();
+
+            frmAddProyect.Show();
+            Close();
+        }
+
+        private void btnSearchProyect_Click(object sender, EventArgs e)
+        {
+            FrmSearcherProyect frmSearcherProyect = new FrmSearcherProyect();
+
+            frmSearcherProyect.Show();
+            Close();
+        }
+    }
+}
