029ad4c39e5b8bd815da7cab53f13c77c837c343
Pelle <joaquinpellegrino@gmail.com>
2018-05-03 15:09:39 -0300

    Agrego comentiarios xml

--- Data/Student.cs
+++ Data/Student.cs
@@ -10,6 +10,12 @@ namespace Data
 {
     public class Student
     {
+        /// <summary>
+        /// Guarda los datos en la DB.
+        /// </summary>
+        /// <param name="dni">Identificador del la persona.</param>
+        /// <param name="fname">Primer nombre de la persona.</param>
+        /// <param name="lname">Apellido de la persona.</param>
         public void CreatePerson(string dni, string fname, string lname)
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(ConnectionSql.CnnString("Pasantia")))
@@ -23,7 +29,12 @@ namespace Data
                 connection.Execute("dbo.spPerson_Insert", parameters, commandType: CommandType.StoredProcedure);
             }
         }
-
+        /// <summary>
+        /// Guarda los datos en la DB.
+        /// </summary>
+        /// <param name="prom">El promedio del estudiante.</param>
+        /// <param name="dni">Identificador de la persona.</param>
+        /// <returns></returns>
         public int CreateStudent(double prom, string dni)
         {
             int idstudent = 0;
@@ -43,7 +54,11 @@ namespace Data
 
             return idstudent;
         }
-
+        /// <summary>
+        /// Guarda los datos en la DB.
+        /// </summary>
+        /// <param name="preference">Preferencia del alumno.</param>
+        /// <param name="idstudent">Identificador del estudiante dentro de la DB.</param>
         public void CreatePreferenceStudent(string preference, int idstudent)
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(ConnectionSql.CnnString("Pasantia")))
@@ -56,6 +71,10 @@ namespace Data
                 connection.Execute("dbo.spPreference_Insert", parameters, commandType: CommandType.StoredProcedure);
             }
         }
+        /// <summary>
+        /// Busca a todas las personas de la DB.
+        /// </summary>
+        /// <returns>Retorna a todas las personas.</returns>
         public List<Entities.Person> GetAllPersons()
         {
 
--- Logic/Student.cs
+++ Logic/Student.cs
@@ -8,13 +8,21 @@ namespace Logic
 {
     public class Student
     {
+        /// <summary>
+        /// Llama a capa de data para guardar los datos en la DB.
+        /// </summary>
+        /// <param name="student">Contiene los datos que se quieren guardar.</param>
         private void CreatePerson(Entities.Student student)
         {
             var dataStudent = new Data.Student();
 
             dataStudent.CreatePerson(student.Dni, student.FirstName, student.LastName);
         }
-
+        /// <summary>
+        /// Llama a capa de data para guardar los datos en la DB.
+        /// </summary>
+        /// <param name="student">Contiene los datos que se quiere guardar.</param>
+        /// <param name="idstudent">El identificador del alumno en la DB.</param>
         private void CreatePreferenceStudent(Entities.Student student, int idstudent)
         {
             var dataStudent = new Data.Student();
@@ -22,7 +30,10 @@ namespace Logic
             foreach (var p in student.Preferences)
                 dataStudent.CreatePreferenceStudent(p, idstudent);
         }
-
+        /// <summary>
+        /// Llama a capa de data para guardar los datos en la DB.
+        /// </summary>
+        /// <param name="student">Contiene los datos que se quiere guardar.</param>
         public void CreateStudent(Entities.Student student)
         {
             var dataStudent = new Data.Student();
@@ -34,13 +45,21 @@ namespace Logic
 
             CreatePreferenceStudent(student, idstudent);
         }
+        /// <summary>
+        /// LLama a la capa de data para buscar datos de la DB.
+        /// </summary>
+        /// <returns>Devuelve una lista de tipo Person.</returns>
         private List<Entities.Person> GetAllPersons()
         {
             var dataStudent = new Data.Student();
 
             return dataStudent.GetAllPersons();
         }
-
+        /// <summary>
+        /// Verifica que el alumno que se quiere crear no exista en la DB.
+        /// </summary>
+        /// <param name="dni">El identificador para saber si esta o no en la DB.</param>
+        /// <returns>Retorna true si lo encontro en la DB y false si no lo encontro.</returns>
         public bool ValidatePerson(string dni)
         {
             var persons = GetAllPersons();
